3a6f4ef26e55818527d4f1db66e9db1098df61bd
Irving <irvingrios20023@gmail.com>
2024-04-11 08:39:58 -0500

    implementacion funciones loading y select player

--- guia_5/GameAsteroid.py
+++ guia_5/GameAsteroid.py
@@ -1,10 +1,5 @@
-import pygame
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db, exceptions
-import os
-import threading
-import random
+import pygame, sys, random, threading,os,firebase_admin, os
+from firebase_admin import db, exceptions,credentials
 
 # Inicialización de Firebase para el jugador 1
 key_path = os.path.join(os.path.dirname(__file__), 'firebase')
@@ -23,7 +18,7 @@ except exceptions.FirebaseError as firebase_error:
 
 # Inicialización de Pygame
 pygame.init()
-
+mi_jugador = 0
 # Configuración de la ventana del juego
 ANCHO, ALTO = 800, 600
 ventana = pygame.display.set_mode((ANCHO, ALTO))
@@ -32,7 +27,7 @@ pygame.display.set_caption("Asteroides")
 # Colores
 BLANCO = (255, 255, 255)
 NEGRO = (0, 0, 0)
-
+font = pygame.font.Font(None, 36)
 # Carga de imágenes
 assest_path = os.path.join(os.path.dirname(__file__), 'assets')
 avion_path = os.path.join(assest_path, 'player_1.png')  # Cambia "avion.png" por la ruta de la imagen del avión del jugador 1
@@ -121,8 +116,8 @@ class Roca(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(ANCHO - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
-        self.speedy = random.randrange(1, 4)  # Reducir la velocidad vertical máxima
-        self.speedx = random.randrange(-1, 1)  # Reducir la velocidad horizontal máxima
+        self.speedy = random.randrange(1,8)  # Reducir la velocidad vertical máxima
+        self.speedx = random.randrange(-3,3)  # Reducir la velocidad horizontal máxima
 
     def update(self):
         self.rect.y += self.speedy
@@ -130,8 +125,8 @@ class Roca(pygame.sprite.Sprite):
         if self.rect.top > ALTO + 10 or self.rect.left < -25 or self.rect.right > ANCHO + 20:
             self.rect.x = random.randrange(ANCHO - self.rect.width)
             self.rect.y = random.randrange(-100, -40)
-            self.speedy = random.randrange(1, 4)  # Reducir la velocidad vertical máxima
-            self.speedx = random.randrange(-1, 1)  # Reducir la velocidad horizontal máxima
+            self.speedy = random.randrange(1,8)  # Reducir la velocidad vertical máxima
+            # self.speedx = 1  # Reducir la velocidad horizontal máxima
 
 
 # Función para manejar eventos de teclado para el jugador 1
@@ -169,6 +164,41 @@ def manejar_eventos_teclado_jugador1(avion_local, todos_sprites, rocas_sprites, 
 
     return True
 
+# Función para manejar eventos de teclado para el jugador 2
+def manejar_eventos_teclado_jugador2(avion_remoto, todos_sprites, rocas_sprites, puntuacion):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+
+    keys = pygame.key.get_pressed()
+
+    # Actualizar las coordenadas del avión local según las entradas del teclado
+    if keys[pygame.K_a]:  # Tecla de flecha izquierda
+        avion_remoto.target_x -= avion_remoto.speed
+    if keys[pygame.K_d]:  # Tecla de flecha derecha
+        avion_remoto.target_x += avion_remoto.speed
+    if keys[pygame.K_w]:  # Tecla de flecha arriba
+        avion_remoto.target_y -= avion_remoto.speed
+    if keys[pygame.K_s]:  # Tecla de flecha abajo
+        avion_remoto.target_y += avion_remoto.speed
+    if keys[pygame.K_k]:  # Tecla de espacio para disparar láser
+        laser = Laser(avion_remoto.rect.centerx, avion_remoto.rect.centery, rocas_sprites, puntuacion)
+        todos_sprites.add(laser)
+
+    # Establecer límites para las coordenadas target
+    avion_remoto.target_x = min(max(avion_remoto.target_x, 0), ANCHO)
+    avion_remoto.target_y = min(max(avion_remoto.target_y, 0), ALTO)
+
+    # Escribir las coordenadas del avión local en Firebase para el jugador 1
+    ref = db.reference('/player_2/-NtXK3NuN1Ly55ArPwUi')
+    ref.update({
+        'x_ship': avion_remoto.target_x,
+        'y_ship': avion_remoto.target_y
+
+    })
+
+    return True
+
 
 # Función para leer las coordenadas del avión remoto desde Firebase
 def leer_coordenadas_jugador2(avion_remoto):
@@ -179,10 +209,71 @@ def leer_coordenadas_jugador2(avion_remoto):
             avion_remoto.target_x = coordenadas_jugador2['x_ship']
             avion_remoto.target_y = coordenadas_jugador2['y_ship']
 
+#Definir pantalla de carga
+def loading_screen():
+    loading = True
+    while loading:
+        value1 = player1.get()
+        value2 = player2.get()
+        player1_selected = value1['-Nsz_waZceu5sMSuXRXq']['active']
+        player2_selected = value2['-NtXK3NuN1Ly55ArPwUi']['active']
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        ventana.fill((0,0,0))
+        loading_text = font.render("Waiting for players. . .",True,(255,255,255))
+        ventana.blit(loading_text, (ANCHO // 2 - loading_text.get_width() // 2, ALTO // 2 - loading_text.get_height() // 2))
+        pygame.display.flip()
 
+        if player1_selected and player2_selected:
+            loading = False
+            print("The players are ready")
+#Seleccionar jugador de forma manual
+def selectPlayer():
+    global mi_jugador
+    value1 = player1.get()
+    value2 = player2.get()
+    player1_selected = value1['-Nsz_waZceu5sMSuXRXq']['active']
+    player2_selected = value2['-NtXK3NuN1Ly55ArPwUi']['active']
+    selected = False
+    
+    while not selected:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    if not player1_selected:
+                        mi_jugador = 1
+                        ref_ship_player1.update({'active': True})
+                        print('Player 1 selected')
+                        selected = True
+                    else:
+                        print("player 1 already exists")
+                if event.key == pygame.K_2:
+                    if not player2_selected:
+                        mi_jugador = 2
+                        ref_ship_player2.update({'active': True})
+                        print('Player 2 selected')
+                        selected = True
+                    else:
+                        print("player 2 already exists")
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+        # lOADING
+        ventana.fill((0, 0, 0))
+        selection_text = font.render("Selected Players:", True, (255, 255, 255))
+        player1_text = font.render("Press '1' to choose player 1", True, (255, 255, 255))
+        player2_text = font.render("Press '2' to choose player 2", True, (255, 255, 255))
+        ventana.blit(selection_text, (ANCHO // 2 - selection_text.get_width() // 2, ALTO // 2 - 50))
+        ventana.blit(player1_text, (ANCHO // 2 - player1_text.get_width() // 2, ALTO // 2))
+        ventana.blit(player2_text, (ANCHO // 2 - player2_text.get_width() // 2, ALTO // 2 + 50))
+        pygame.display.flip()
 # Función principal del juego
 def main():
-    mi_jugador = 1  # Este es el jugador actual, cambia esto a 2 si eres el Jugador 2
+    # Inicializacion de mi jugador
+    selectPlayer()
+    loading_screen()
     avion_local = Avion(mi_jugador, ANCHO // 2, ALTO // 2)  # Crear avión local
     avion_remoto = Avion(2 if mi_jugador == 1 else 1, ANCHO // 2, ALTO // 2)  # Crear avión remoto
 
@@ -206,6 +297,8 @@ def main():
     while jugando:
         if mi_jugador == 1:
             jugando = manejar_eventos_teclado_jugador1(avion_local, todos_sprites, rocas_sprites, puntuacion)
+        elif mi_jugador == 2:
+            jugando = manejar_eventos_teclado_jugador2(avion_remoto, todos_sprites, rocas_sprites, puntuacion)
 
         ventana.fill((0, 0, 0))  # Limpia la ventana antes de redibujar los aviones
         avion_local.update()
@@ -221,14 +314,16 @@ def main():
         rocas_sprites.draw(ventana)
 
         # Mostrar puntuación en pantalla
-        font = pygame.font.Font(None, 36)
         texto_puntuacion = font.render("Puntuación: " + str(puntuacion.puntos), True, BLANCO)
         ventana.blit(texto_puntuacion, (10, 10))
 
         pygame.display.flip()  # Actualizar la pantalla
-
     pygame.quit()
-
-
+ref_ship_player1.update({'active':False})
+ref_ship_player2.update({'active':False})
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"Se produjo un error en el juego: {e}")
+        pygame.quit()
